39932d54ecf9bac930431d5e1d2e8d71f1e869f1
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-01-18 22:57:57 +0300

    [lab7] add basic test

--- tests/test.main.cpp
+++ tests/test.main.cpp
@@ -1,1 +1,21 @@
 #include "pch.h"
+#include "../src/TaskTable.h"
+#include "../src/LLParser.h"
+
+TEST_CASE("SUCCESS testies")
+{
+	std::string expected;
+	std::string actual;
+
+	std::istringstream iss;
+
+	TaskTable table;
+	LLParser parser(iss, table);
+
+	SECTION("Check termianl a")
+	{
+		iss.str("-(8)");
+
+		REQUIRE(parser.Parse());
+	}
+}
