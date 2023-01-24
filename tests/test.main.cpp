fadb94e0c09ef62bab4bb8c61e20e5b294e5d071
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-01-24 16:38:53 +0300

    [lab7] add new table rules

--- tests/test.main.cpp
+++ tests/test.main.cpp
@@ -11,133 +11,133 @@ TEST_CASE("SUCCESS testies")
 
 	SECTION("Check terminal a")
 	{
-		iss.str("a");
+		iss.str("a\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check terminal b")
 	{
-		iss.str("b");
+		iss.str("b\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check terminal 8")
 	{
-		iss.str("8");
+		iss.str("8\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check terminal 3")
 	{
-		iss.str("3");
+		iss.str("3\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check terminal 3 with minus")
 	{
-		iss.str("-3");
+		iss.str("-3\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check terminal b with minus")
 	{
-		iss.str("-b");
+		iss.str("-b\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check brackets")
 	{
-		iss.str("()");
+		iss.str("()\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check nested pair of brackets")
 	{
-		iss.str("(())");
+		iss.str("(())\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check terminal in brackets")
 	{
-		iss.str("(a)");
+		iss.str("(a)\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check terminal with minus in brackets")
 	{
-		iss.str("(-a)");
+		iss.str("(-a)\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check sum in brackets")
 	{
-		iss.str("(-a)+(a)");
+		iss.str("(-a)+(a)\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check sum inside brackets")
 	{
-		iss.str("(-a+b)+(a)");
+		iss.str("(-a+b)+(a)\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check multiplication inside brackets")
 	{
-		iss.str("(-a+b)+(a*b)");
+		iss.str("(-a+b)+(a*b)\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check sum of brackets and non-brackets")
 	{
-		iss.str("(-a+b)+a");
+		iss.str("(-a+b)+a\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check multiplication of brackets and non-brackets")
 	{
-		iss.str("a*(-a+b)");
+		iss.str("a*(-a+b)\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check sequence of non-brackets operations")
 	{
-		iss.str("a*b+3*8+a");
+		iss.str("a*b+3*8+a\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check minus in a sequence of non-brackets operations")
 	{
-		iss.str("a*-8+a");
+		iss.str("a*-8+a\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check minus before brackets")
 	{
-		iss.str("-(a*b+3)*-8+a");
+		iss.str("-(a*b+3)*-8+a\n");
 
 		REQUIRE(parser.Parse());
 	}
 
 	SECTION("Check nested brackets in binary operation")
 	{
-		iss.str("-((((a))))*-8+a");
+		iss.str("-((((a))))*-8+a\n");
 
 		REQUIRE(parser.Parse());
 	}
@@ -153,12 +153,12 @@ TEST_CASE("Error tests")
 	SECTION("Unbalanced brackets sequences")
 	{
 		std::vector<std::string> unbalancedSequences = {
-			"(",
-			")",
-			"(()",
-			"())",
-			"(b",
-			"b)",
+			"(\n",
+			")\n",
+			"(()\n",
+			"())\n",
+			"(b\n",
+			"b)\n",
 		};
 		
 		for (auto&& sequence : unbalancedSequences)
@@ -176,12 +176,12 @@ TEST_CASE("Error tests")
 	SECTION("Incorrect combination of params")
 	{
 		std::unordered_map<std::string, size_t> combinations = {
-			{ "b*bb", 3 },
-			{ "()()", 2 },
-			{ "((a*))8", 4 }, // FIXED: ошибка в позиции 6, хотя вроде логичнее сразу после * ее получать?
-			{ "88*3", 1 },
-			{ "*b", 0 },
-			{ "8*", 2 }, // FIXED: тест не должен падать
+			{ "b*bb\n", 3 },
+			{ "()()\n", 2 },
+			{ "((a*))8\n", 4 }, // FIXED: ошибка в позиции 6, хотя вроде логичнее сразу после * ее получать?
+			{ "88*3\n", 1 },
+			{ "*b\n", 0 },
+			{ "8*\n", 2 }, // FIXED: тест не должен падать
 		};
 
 		for (auto&& [combination, errorPos] : combinations)
